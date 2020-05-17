7ec4a00c114d6b699c81f0e5ba3f44074ed2f36a
DubiBerlin <woose@web.de>
2020-05-17 19:37:13 +0200

    move transition code to App.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,18 +23,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.fade-enter {
-  opacity: 0;
-}
-
-.fade-leave-to {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease-out;
-}
-</style>
