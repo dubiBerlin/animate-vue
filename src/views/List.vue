91c278653294899aa85f49380374a3497e783999
DubiBerlin <woose@web.de>
2020-05-17 21:25:47 +0200

    avoid duplicate entries

--- src/views/List.vue
+++ src/views/List.vue
@@ -8,6 +8,9 @@
         {{ contact }}
       </li>
     </ul>
+    <div v-if="isError" class="error">
+      <span>{{this.newContact}}</span> allready exists in the list 
+    </div>
   </div>
 </template>
 
@@ -15,15 +18,40 @@
 export default {
   data() {
     return {
+      isError:false,
       newContact: '',
       contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Vunderlind']
     }
   },
   methods: {
     addContact() {
-      this.contacts.push(this.newContact)
-      this.newContact = ''
+      if(this.newContact.length==0){
+        return;
+      } 
+      if( this.contacts.find(contact => contact==this.newContact)===undefined){
+        this.contacts.push(this.newContact)
+        this.newContact = ''
+        this.isError = false;
+      }else{
+        this.isError = true;
+      }
     }
   }
 }
 </script>
+<style>
+  .error {
+    color: crimson;
+    font-size: 14px;
+    font-weight: 700;
+    text-align: center;
+    border: 1px solid crimson;
+    border-radius: 5px;
+    padding: 10px;
+    margin-top: 10px;
+  }
+
+  .error > span {
+    font-weight: 900;
+  }
+</style>
