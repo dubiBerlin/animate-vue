235e95eaa4abc8b79f28905da068bccd03f006da
DubiBerlin <woose@web.de>
2020-05-18 08:54:31 +0200

    add watcher to watch changes for v-model

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,6 +2,7 @@
   <div>
     <input type="text" v-model="newContact" placeholder="Name" />
     <button @click="addContact">Add Contact</button>
+    <button @click="blinkList">Blink</button>
     <transition name="fade">
       <div v-show="isError" class="error">
         <span>{{ this.newContact }}</span> allready exists in the list
@@ -9,11 +10,14 @@
     </transition>
 
     <ul>
-      <li v-for="contact in contacts" :key="contact">
-        {{ contact }}
+      <li
+        v-for="contact in contacts"
+        :key="contact.name"
+        :class="{ errorli: contact.isDuplicate }"
+      >
+        {{ contact.name }}
       </li>
     </ul>
-    
   </div>
 </template>
 
@@ -21,22 +25,64 @@
 export default {
   data() {
     return {
-      isError: false,
+      duplicateContact: "",
       newContact: "",
-      contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Vunderlind']
+      contacts: [
+        {
+          name: "Beau Thabeast",
+          isDuplicate: false,
+          blink: false
+        },
+        {
+          name: "Alice Vunderlind",
+          isDuplicate: false,
+          blink: false
+        },
+        {
+          name: "Cindy Rella",
+          isDuplicate: false,
+          blink: false
+        }
+      ],
+      liErrorClass: "error-li",
+      blinkClass: "blink",
+      blink: false,
+      isError: false
+    }
+  },
+  watch: {
+    newContact: function(newValue, oldValue) {
+      if (this.isError) {
+        this.isError = newValue != oldValue ? false : true
+        this.duplicateContact.isDuplicate = false
+        this.duplicateContact = ""
+      }
     }
   },
   methods: {
+    blinkList() {
+      console.log("blinkList() ", this.blink)
+      // this.blink = true
+    },
     addContact() {
-      if(this.newContact.length == 0){
-        return;
+      if (this.newContact.length == 0) {
+        return
       } 
-      if(this.contacts.find(contact => contact==this.newContact) === undefined){
-        this.contacts.push(this.newContact)
+
+      let contact = this.contacts.find(
+        contact => contact.name == this.newContact
+      )
+      if (contact === undefined) {
+        this.contacts.push({
+          name: this.newContact,
+          isDuplicate: false,
+          blink: false
+        })
         this.newContact = ""
-        this.isError = false
-      }else{
+      } else {
         this.isError = true
+        contact.isDuplicate = true
+        this.duplicateContact = contact
       }
     }
   }
@@ -58,5 +104,39 @@ export default {
 
 .error > span {
   font-weight: 900;
+}
+
+.errorli {
+  background-color: crimson;
+  color: #eee;
+  transition: all 1s;
+}
+
+.errorli:hover {
+  color: #eee;
+  border-color: crimson;
+}
+
+.blink {
+  border-color: goldenrod;
+  background-color: gold;
+  color: #000;
+  transition: all 0.2s;
+}
+
+li {
+  background-color:white;
+  width: 30%;
+  margin: 5px auto 0 auto;
+  padding: 5px;
+  border: 1px solid transparent;
+  border-radius: 5px;
+  color: #000;
+}
+
+li:hover {
+  border-color: #eee;
+  color: darkgray;
+  transition: all 0.2s;
 }
 </style>
